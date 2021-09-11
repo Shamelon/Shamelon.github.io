d49b5ca5496aa0a6276a424932c75ca805c49c1e
Shamelon <88483067+Shamelon@users.noreply.github.com>
2021-09-11 21:52:31 +0300

    Познакомился с Java Script

--- src/js/test.js
+++ src/js/test.js
@@ -45,3 +45,35 @@ while (n < 4)
 for(let count = 0; count < 5; count += 2){
     console.log(count * 3)
 }
+
+let getsum = (a, b) => a + b;
+
+console.log(getsum(10, 23));
+
+confirm('push ok')
+
+alert('PUSH OK!')
+
+prompt("How are you old?", "5")
+
+console.log('ARRAY');
+
+let matrix=[
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8]
+]
+
+console.log(matrix[1][1])
+
+matrix.push([9, 10, 11]) //end
+
+matrix.unshift([-3, -2, -1]) //start
+
+console.log(matrix) 
+
+matrix.pop(); //end
+
+matrix.shift() //start
+
+console.log(matrix)
